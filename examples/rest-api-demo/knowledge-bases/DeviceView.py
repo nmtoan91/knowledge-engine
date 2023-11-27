3a04532ce6b3ec853cf705de9891d0cf191ab4cf
nmtoan91 <nmtoan91@gmail.com>
2023-11-27 15:40:21 +0900

    savc

---
+++ examples/rest-api-demo/knowledge-bases/DeviceView.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+from tkinter import ttk
+import threading 
+from PIL import Image, ImageTk
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
+
+class DeviceView(tk.Frame):
+    
+
+    def __init__(self,root, id):
+        self.id = id
+        self.isFlexible = False
+        self.isOperation = False
+        self.data_x = []
+        self.data_y = []
+
+        frame = tk.Frame(root, bg='lavender', width=300, height=600, pady=10,padx = 10)
+        frame.grid_rowconfigure(1, weight=1)
+        frame.grid_columnconfigure(0, weight=1)
+        image = Image.open(r"examples/rest-api-demo/knowledge-bases/Images/lightbulb.png").resize((100, 100))
+        my_img = ImageTk.PhotoImage(image)
+        my_label = tk.Label(frame,image=my_img)  
+        my_label.img = my_img  
+        my_label.grid( row=0,column=0, sticky=tk.N)
+
+        frame_buttons = tk.Frame(frame, bg='cyan', width=100, height=100, pady=10,padx = 20)
+        frame_buttons.grid( row=2,column=0, sticky=tk.N)
+
+        
+        self.button_flexible = tk.Button(frame_buttons, text="Flexible: OFF", fg="black", command=self.flexible_click)
+        self.button_flexible.pack(side=tk.LEFT)
+
+        self.button_operation = tk.Button(frame_buttons, text="Operation", command=self.operation_click)
+        self.button_operation.pack(side=tk.LEFT)
+        self.UpdateUI_button_operation()
+
+        #bluebutton = tk.Button(frame_buttons, text="Blue", fg="blue")
+        #bluebutton.pack(side=tk.LEFT)
+
+
+
+        self.datalabel = tk.Label(frame,text="dmo sdf sdfsdf sdf sdfsd dsf")
+        self.datalabel.grid( row=6,column=0, sticky=tk.S)
+
+        self.frame = frame
+
+        #self.figure = plt.figure(figsize=(2, 2))
+        self.fig = plt.figure(figsize=(2, 2))
+        self.ax = self.fig.add_subplot(111)
+        
+
+        x = np.linspace(-1, 1, 50)
+        y = 2*x + 1
+        self.ax.plot(x, y)
+        self.scatter = FigureCanvasTkAgg(self.fig, frame)
+        self.scatter.get_tk_widget().grid( row=4,column=0, sticky=tk.S)
+
+
+        self.scale = tk.Scale(frame, from_=0, to=100, orient=tk.HORIZONTAL)
+        self.scale.grid( row=8,column=0, sticky=tk.S)
+
+
+    def RevieveData(self, data):
+        print('ddd', type(data['temperature']))
+
+
+        self.data_x.append(len(self.data_x))
+        self.data_y.append(float(data['temperature']))
+        if len(self.data_y) > 100: self.data_x.pop(0);self.data_y.pop(0)
+
+        self.UpdateUI_Chart()
+
+    def UpdateUI_Chart(self):
+        self.ax.clear()
+        self.ax.plot(self.data_x,self.data_y )
+        self.scatter.draw()
+
+    
+    def flexible_click(self):
+        self.isFlexible = not self.isFlexible
+        self.UpdateUI_button_flexible()
+    def operation_click(self):
+        self.isOperation = not self.isOperation
+        self.UpdateUI_button_operation()
+
+    def UpdateUI_button_flexible(self):
+        if self.isFlexible:
+            self.button_flexible.config(text="Flexible: ON", fg="green")
+        else:
+            self.button_flexible.config(text="Flexible: OFF", fg="black")
+    def UpdateUI_button_operation(self):
+        if self.isOperation:
+            self.button_operation.config(text="Operation: ON", fg="green")
+        else:
+            self.button_operation.config(text="Operation: OFF", fg="black")
---
+++ examples/rest-api-demo/knowledge-bases/MainView.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+import threading 
+from PIL import Image, ImageTk
+from DeviceView import DeviceView
+
+class MainView(threading.Thread):
+    def __init__(self, thread_name, thread_ID): 
+        threading.Thread.__init__(self) 
+        self.thread_name = thread_name 
+        self.thread_ID = thread_ID 
+
+    def run(self):
+        self.devices = {}
+        root = tk.Tk()
+        root.geometry("600x800")
+        root.title('Demostation')
+        root.geometry('{}x{}'.format(600, 500))
+
+        root.columnconfigure(0, weight=1)
+        root.columnconfigure(1, weight=1)
+
+        frame1  = DeviceView(root,"<https://example.org/sensor/1>")
+        frame2  = DeviceView(root,"none")
+        frame1.frame.grid(row=0, column =0)
+        frame2.frame.grid(row=0, column =1)
+
+        self.devices[frame1.id] = frame1
+        self.devices[frame2.id] = frame2
+
+        root.mainloop()
+    def RevieveData(self, data):
+        if data['sensor']  in self.devices:
+            self.devices[data['sensor']].RevieveData(data)
+    
+if __name__ == "__main__":
+    mainView = MainView("My tkinter thread", 1000) 
+    mainView.start() 
